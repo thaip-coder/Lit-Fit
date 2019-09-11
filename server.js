209b2185ba2180362184e37012255a3cc166a4bc
Thai Phan <thaiphan90@yahoo.com>
2019-09-10 20:56:16 -0500

    remove handlebar code from server.js

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
