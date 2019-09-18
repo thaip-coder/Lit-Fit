902c9ef27a672bddd97a765b319e1ba136e50da3
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-09-17 20:36:40 -0500

    enlist error fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,33 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all books
-  app.get("/api/all", function(req, res) {
-    db.Books.findAll({}).then(function(result) {
+  app.get("/api/all", function (req, res) {
+    db.Books.findAll({}).then(function (result) {
       res.json(result);
     });
   });
   // Get books within a specified category
-  app.get("/api/:category", function(req, res){
+  app.get("/api/:category", function (req, res) {
     db.Books.findAll({
       where: {
         category: req.params.category
       }
-    }).then(function(result){
-      res.json(result)
+    }).then(function (result) {
+      res.json(result);
     });
   });
 
   // Create a new example
-  app.post("/api/books", function(req, res) {
-    db.Books.create(req.body).then(function(dbBooks) {
+  app.post("/api/books", function (req, res) {
+    db.Books.create(req.body).then(function (dbBooks) {
       res.json(dbBooks);
     });
   });
 
   // Delete a book by id
-  app.delete("/api/:id", function(req, res) {
-    db.Books.destroy({ where: { id: req.params.id } }).then(function(result) {
+  app.delete("/api/:id", function (req, res) {
+    db.Books.destroy({ where: { id: req.params.id } }).then(function (result) {
       res.json(result);
     });
   });
