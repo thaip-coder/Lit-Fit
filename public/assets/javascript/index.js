7d18b229e78bd5095b9537401a001839ba783a74
Thai Phan <thaiphan90@yahoo.com>
2019-09-20 16:43:04 -0500

    added login modal

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,27 +1,9 @@
-// accordian code
+// Login Modal
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-var name = document.getElementsByClassName("nameInp");
-
-var readInp = document.getElementsByClassName("inputs");
 
 //readInp.createTextNode(name);
 
